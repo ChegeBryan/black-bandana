42baf45d165ff17e2ef7e7dbe1073c084f193ac1
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-18 18:02:23 +0300

    [Chore #163261980] Instantiate manager

--- manage.py
+++ manage.py
@@ -2,7 +2,11 @@
 
 import os
 
+from flask_script import Manager
 
 from app import create_app
 
 app = create_app(os.getenv('APP_SETTING') or 'production')
+
+# Instantiate manager with flask instance
+manager = Manager(app)
