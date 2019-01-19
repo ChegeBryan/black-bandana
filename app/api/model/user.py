3820924eff50820473cfe7355f30afbeafd7af61
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-19 11:06:37 +0300

    [Feature #163261740] User class skeleton

---
+++ app/api/model/user.py
@@ -0,0 +1,12 @@
+""" User models """
+
+import uuid
+
+
+class User:
+    """
+    User model class 
+    """
+    def __init__(self, user_name, user_email, password):
+        """ Instantiates the user class """
+        pass
