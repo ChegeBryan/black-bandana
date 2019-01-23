aeb14036662d3dfcd7e9e32f2821b957f9f74ad9
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-23 15:26:35 +0300

    [Feature #163261740] User with malformed email object

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -14,9 +14,10 @@ class BaseTestData(unittest.TestCase):
         """ Initialize app and the test data """
         self.app = create_app('testing')
 
-        self.user = User(user_name='name', user_email='anonymous@gmail.com', password='password')
+        self.user = User(user_name='name', user_email='anonymous@gmail.com', password='password23')
         self.user_holder = self.user.display_user_holder()
         self.null_username = User(user_name='', user_email='anonymous@gmail.com', password='password')
         self.null_username_holder = self.null_username.display_user_holder()
-        
+        self.malformed_email = User(user_name='name', user_email='anoy.com', password='password21')
+        self.malformed_email_holder = self.malformed_email.display_user_holder()
 
