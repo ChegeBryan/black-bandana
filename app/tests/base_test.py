91ab9c2b06bfc27d3bfb96ec4a9ef718afa29039
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-23 15:15:51 +0300

    [Feature #163261740] Add valid user properties

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -3,6 +3,7 @@
 import unittest
 
 from app import create_app
+from app.api.model.user import User
 
 
 class BaseTestData(unittest.TestCase):
@@ -12,3 +13,7 @@ class BaseTestData(unittest.TestCase):
     def setUp(self):
         """ Initialize app and the test data """
         self.app = create_app('testing')
+
+        self.user = User(user_name='name', user_email='anonymous@gmail.com', password='password')
+        self.user_holder = self.user.display_user_holder()
+        
