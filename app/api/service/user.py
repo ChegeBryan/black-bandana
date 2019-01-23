2d21abe4ac4ed6f5c3d5ba677d55065ddb0cb2e5
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-23 16:44:27 +0300

    [Feature #163261740] Add new user object

--- app/api/service/user.py
+++ app/api/service/user.py
@@ -1,15 +1,24 @@
 """ Methods for data manipulation in the Mock db """
 
 from app.api.db.mock_db import MockDB
+from app.api.model.user import User
 
 
 def save_new_user(data):
     """
     Save new user function 
     """
-    pass
+    username =  data['user_name']
+    user_email = data['user_email']
+    password = data['password']
 
-    
+    new_user = User(
+        user_name=username,
+        user_email=user_email,
+        password=password
+    )
+
+
 
 def save_changes(data):
     MockDB.USERS.append(data)
