cea47f9d7593512930a84d329571ddb2466168ee
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-23 10:57:14 +0300

    [Feature #163261740] Method to return user object attributes

--- app/api/model/user.py
+++ app/api/model/user.py
@@ -9,4 +9,15 @@ class User:
     """
     def __init__(self, user_name, user_email, password):
         """ Instantiates the user class """
-        pass
+        self.user_id = str(uuid.uuid4())
+        self.user_name = user_name
+        self.user_email = user_email
+        self.password = password
+    
+    def display_user_holder(self):
+        return {
+            'user_id': self.user_id,
+            'user_name': self.user_name,
+            'user_email': self.user_email,
+            'password': self.password
+        }
