84b4fdd026d7911db0881fd79d9925e59303cafc
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-24 16:01:15 +0300

    [Bug] Add commas on user_details

--- app/api/util/dto.py
+++ app/api/util/dto.py
@@ -9,8 +9,8 @@ class UserDto:
     """
     api = Namespace('user', description='user related operations')
     user = api.model('user_details',{
-        'user_name': fields.String(required=True, description="User's username")
-        'user_email': fields.String(required=True, description="User email address")
-        'password': fieldsString(required=True, description="User password")
+        'user_name': fields.String(required=True, description="User's username"),
+        'user_email': fields.String(required=True, description="User email address"),
+        'password': fields.String(required=True, description="User password"),
         'user_uri': fields.String(description='User identifier')
     })
