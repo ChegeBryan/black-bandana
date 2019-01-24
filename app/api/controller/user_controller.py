bfeab45bb3a82fe5b828a6cd0120af12aac0d7ff
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-24 14:48:38 +0300

    [Feature #163261740] Add users api route

---
+++ app/api/controller/user_controller.py
@@ -0,0 +1,19 @@
+""" User api endpoints """
+
+from flask import request
+from flask_restplus import Resource
+
+from ..util.dto import UserDto
+from ..service.user import save_new_user
+
+
+api = UserDto.api
+_user = UserDto.user
+
+
+@api.route('/users')
+class Users(Resource):
+    """
+    User resource for the API
+    """
+    pass
