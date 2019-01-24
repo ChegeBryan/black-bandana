d5f8e8235f0a28272ce685b1f07dc0aa67f13eaf
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-24 14:12:56 +0300

    [Feature #163261740] Instantiate and register the user to Namespace

---
+++ app/api/util/dto.py
@@ -0,0 +1,16 @@
+"""dto.py carries data between api calls """
+
+from flask_restplus import Namespace, fields
+
+
+class UserDto:
+    """"
+    User Data Transfer Object
+    """
+    api = Namespace('user', description='user related operations')
+    user = api.model('user_details',{
+        'user_name': fields.String(required=True, description="User's username")
+        'user_email': fields.String(required=True, description="User email address")
+        'password': fieldsString(required=True, description="User password")
+        'user_uri': fields.String(description='User identifier')
+    })
