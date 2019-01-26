f6f5fdce6f2192a49ee3b257883dee81a05537b3
Chege Bryan <cbbryan8@gmaiil.com>
2019-01-26 14:12:11 +0300

    [Feature #163261740] Refactor base test data for int username

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -27,7 +27,7 @@ class BaseTestData(unittest.TestCase):
         self.null_password_holder = self.null_password.display_user_holder()
         self.null_user_entries = User(user_name='', user_email='', password='')
         self.null_user_entries_holder = self.null_user_entries.display_user_holder()
-        self.int_username = User(user_name=3222, user_email='anonymous@gam.com', password='password23')
+        self.int_username = User(user_name="3222", user_email='anonymous@gam.com', password='password23')
         self.int_username_holder = self.int_username.display_user_holder()
         self.password_length = User(user_name='name', user_email='anonymous@gm.com', password='passw')
         self.password_length_holder = self.password_length.display_user_holder()
